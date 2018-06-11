f51a56063241371e24934841950b4d80ed558c5f
nishantvyas <nishantvyas@gmail.com>
2018-06-11 14:25:19 -0700

    hello world python program

---
+++ hello.py
@@ -0,0 +1,16 @@
+'''
+Hello World Python Program
+'''
+
+def helloworld():
+	'''
+	Simple Hello World Function
+	'''
+	print("Hello World!")
+
+
+if __name__ == "__main__":
+	'''
+	main function
+	'''
+	helloworld()
