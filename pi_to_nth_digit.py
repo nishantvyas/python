10a3c7f7ec1c7dbb18c340f0c84eaaba71a5b812
nishantvyas <nishantvyas@gmail.com>
2018-06-13 14:23:16 -0700

    nth digit formater for pi

---
+++ pi_to_nth_digit.py
@@ -0,0 +1,23 @@
+'''
+Find PI to the Nth Digit - Enter a number and have the program generate π (pi) up to that many decimal places. Keep a limit to how far the program will go.
+
+'''
+import math
+
+def pi_to_nth_digit(precision=2):
+    
+    """
+    > n = 4
+    > p = math.pi
+    > '{0:.{1}f}'.format(p, n)
+    > '3.1416'
+    the nested {1} takes the second argument, the current value of n, and applies it as specified (here, to the "precision" part of the format --
+    number of digits after the decimal point), and the outer resulting {0:.4f} then applies.
+    """
+
+    return '{number:.{digits}f}'.format(number=math.pi, digits=precision)
+
+if __name__ == "__main__":
+
+    my_pi = pi_to_nth_digit(precision=3)
+    print(my_pi)
