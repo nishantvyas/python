5a948557511ee2daee623ea2f9edaf7dd13cb36e
nishantvyas <nishantvyas@gmail.com>
2018-06-17 15:40:19 -0700

    reverse the string

---
+++ reverse_string.py
@@ -0,0 +1,13 @@
+"""
+Reverse a String - Enter a string and the program will reverse it and print it out.
+"""
+
+def reverse_string(string=None):
+    return string[::-1]
+
+if __name__ == "__main__":
+    """
+    """
+    print(reverse_string("Nishant"))
+    print(reverse_string("Hello World!"))
+    print(reverse_string("madam"))
