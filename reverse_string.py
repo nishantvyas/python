598429a0b6c7c62c975aecebcdff4936cce12ff8
nishantvyas <nishantvyas@gmail.com>
2018-06-17 21:05:58 -0700

    reverse the string

--- reverse_string.py
+++ reverse_string.py
@@ -5,9 +5,19 @@ Reverse a String - Enter a string and the program will reverse it and print it 
 def reverse_string(string=None):
     return string[::-1]
 
+def is_pelindrome(string=None):
+    if string == string[::-1]:
+        return True
+    else:
+        return False
+
 if __name__ == "__main__":
     """
     """
+
     print(reverse_string("Nishant"))
     print(reverse_string("Hello World!"))
     print(reverse_string("madam"))
+
+    print(is_pelindrome("madam"))
+    print(is_pelindrome("nishant"))
