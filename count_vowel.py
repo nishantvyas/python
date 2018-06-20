e6e240bf2c3eeeec6acc5db614d37f43fcb378d6
nishantvyas <nishantvyas@gmail.com>
2018-06-20 11:37:40 -0700

    report a sum of each vowel found

--- count_vowel.py
+++ count_vowel.py
@@ -11,6 +11,7 @@ def count_vowel(string=None):
     """
     vowel = ['a','e','i','o','u']
     vowel_count = 0
+    vowel_count_dict = {'a':0,'e':0,'i':0,'o':0,'u':0}
 
     if string == None:
         print("Enter valid staring")
@@ -19,8 +20,10 @@ def count_vowel(string=None):
     for char in string:
         if char in vowel:
             vowel_count += 1
+            vowel_count_dict[char] += 1
 
     print(f"Total vowels are: {vowel_count}")
+    print(f"Count for each vowel {vowel_count_dict}")
 
 if __name__ == "__main__":
     """
