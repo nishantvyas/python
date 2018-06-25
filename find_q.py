85683555044b99bc4d91fd8a8a4d07750becd53c
nishantvyas <nishantvyas@gmail.com>
2018-06-25 12:55:28 -0700

    find q

---
+++ find_q.py
@@ -0,0 +1,33 @@
+"""
+Write a program that calculates and prints the value according to the given formula:
+Q = Square root of [(2 * C * D)/H]
+Following are the fixed values of C and H:
+C is 50. H is 30.
+D is the variable whose values should be input to your program in a comma-separated sequence.
+Example
+Let us assume the following comma separated input sequence is given to the program:
+100,150,180
+The output of the program should be:
+18,22,24
+"""
+
+import math
+
+def find_q(var_D=1):
+    """
+
+    :param var_D:
+    :return:
+    """
+    var_C = 50
+    var_H = 30
+    return round(math.sqrt((2 * var_C * var_D)/ var_H))
+
+if __name__ == "__main__":
+
+    input_number = "100,150,180"
+    result_list = []
+    for number in input_number.split(','):
+        result_list.append(find_q(int(number)))
+
+    print(result_list)
