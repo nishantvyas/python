0b4b48c9ef8beba8110c2d4eeebb20200735cd66
nishantvyas <nishantvyas@gmail.com>
2018-06-27 11:55:49 -0700

    Check even digits

--- even_digits.py
+++ even_digits.py
@@ -4,16 +4,16 @@ Write a program, which will find all such numbers between 1000 and 3000 (both in
 The numbers obtained should be printed in a comma-separated sequence on a single line.
 """
 
-def allEvenDigits(numberasString):
+def checkEvenDigits(string):
     """
     This functions finds if all digits of a number is even or not.
     :param number:
     :return boolean:
     """
-    if isinstance(numberasString,int):
-        numberasString = str(numberasString)
+    if isinstance(string,int):
+        string = str(string)
 
-    for digit in numberasString:
+    for digit in string:
         if not int(digit) % 2 == 0:
             return False
 
@@ -25,9 +25,22 @@ if __name__ == "__main__":
     """
 
     ## Code to check if given number has all even digits
-    
+    """
     number = 200
-    if allEvenDigits(str(number)):
+    if checkEvenDigits(str(number)):
         print(f"{number} has all even digits")
     else:
         print(f"{number} does NOT have all even digits")
+    """
+
+    ##find all numbers between 1000 and 2000 with all even digits
+    all_even_digits = []
+    min_range = 1000
+    max_range = 2600
+
+    for number in range(min_range,max_range):
+        if checkEvenDigits(number):
+            all_even_digits.append(number)
+
+    print(f"List of all event digits between {min_range} and {max_range}")
+    print(all_even_digits)
