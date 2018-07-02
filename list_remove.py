ea270141167a5a94c2be95e41315ca184eeeca91
nishantvyas <nishantvyas@gmail.com>
2018-07-02 11:47:06 -0700

    list remove items methods

--- list_remove.py
+++ list_remove.py
@@ -20,8 +20,19 @@ def list_remove_native(listObj, item):
 
     return listObj
 
+def list_remove_fast(listObj, item):
+    """
+
+    :param listObj:
+    :param item:
+    :return listObj:
+    """
+
+    return [x for x in listObj if x!=24 ]
+
 if __name__ == "__main__":
     """
     """
     my_list = [12,24,35,24,88,120,155]
     print(list_remove_native(my_list, 24))
+    print(list_remove_fast(my_list, 24))
