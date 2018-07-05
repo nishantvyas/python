52d0297a7c16be00a029d4ff4eeba8defc5a9fc1
nishantvyas <nishantvyas@gmail.com>
2018-07-05 15:50:54 -0700

    binary search on sorted list

--- binary_search_list.py
+++ binary_search_list.py
@@ -12,9 +12,36 @@ def bin_search_default(list, element):
 
     return list.index(element)
 
+def bin_search_new(list, element):
+    """
+
+    :param list:
+    :param element:
+    :return index:
+    """
+
+    index = -1
+    top_of_list = len(list) - 1
+    bottom_of_list = 0
+
+    while top_of_list > bottom_of_list and index == -1:
+        mid_of_list = int((top_of_list + bottom_of_list) /2)
+        ##print(f"top:{top_of_list}    bottom:{bottom_of_list}    mid:{mid_of_list}")
+        if list[mid_of_list] == element:
+            index = mid_of_list
+            return index
+        elif list[mid_of_list] > element:
+            top_of_list = mid_of_list
+        else:
+            bottom_of_list = mid_of_list
+
+    return index
+
 if __name__ == "__main__":
     """
     """
     list = [2,5,7,9,11,17,222]
 
+    ##find the index of element 11 in list
     print(bin_search_default(list, 11))
+    print(bin_search_new(list, 11))
