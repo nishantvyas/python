febd89baf9f6c994a7f19157a3aad01fb455076e
nishantvyas <nishantvyas@gmail.com>
2018-07-10 11:20:36 -0700

    inventory app

--- inventory.py
+++ inventory.py
@@ -8,28 +8,45 @@ class product():
     """
     blue print for product information
     """
-    def __init__(self, id,name, price, unit):
+    def __init__(self, id, name, price, unit):
         """
         init default variables for product/item i.e. name, id, price
         """
+
         self.id = id
         self.name = name
         self.price = price
         self.unit = unit
         self.item_json = {"id":self.id, "name":self.name, "price":self.price, "unit": self.unit}
 
-    def getProductDetail(self, **kwargs):
+    def getProductDetail(self, *args):
         """
         get detail of one or more attributes of product
         :return:
         """
-        if len(kwargs) == 0:
-            print(self.item_json)
-            
+        return_json = {}
+
+        if len(args) == 0:
+            return self.item_json
+        else:
+            for arg in args:
+                if arg == "id":
+                    return_json[arg]=self.id
+                elif arg=="name":
+                    return_json[arg] = self.name
+                elif arg == "price":
+                    return_json[arg] = self.price
+                elif arg=="unit":
+                    return_json[arg] = self.unit
+                else:
+                    return_json = None
+
+            return return_json
 
 
-if __name__ == "__main__":
+
+if __name__ == "__main__"   :
     """
     """
     banana = product(1000, "banana", 1.99, "pound")
-    banana.getProductDetail()
+    print(banana.getProductDetail("name", "id","unit"))
